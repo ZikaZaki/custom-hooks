970be2bf777c3f8c9fd30f775b30a43b64928680
ZikaZaki <17160061@su.edu.ye>
2023-05-31 00:55:09 +0300

    add CounterOne component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import CounterOne from "./components/CounterOne";
 import DocTitleOne from "./components/DocTitleOne";
 import DocTitleTwo from "./components/DocTitleTwo";
 
 function App() {
   return (
     <div className="App">
-      <DocTitleOne />
-      <DocTitleTwo />
+      {/* <DocTitleOne />
+      <DocTitleTwo /> */}
+      <CounterOne />
     </div>
   );
 }
