311a61409cc0e004fe8882ebafe614129ebeeb7c
ZikaZaki <17160061@su.edu.ye>
2023-05-31 00:37:19 +0300

    add useState & useEffect hooks

--- src/components/DocTitleOne.js
+++ src/components/DocTitleOne.js
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 function DocTitleOne() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `Count ${count}`
+  }, [count])
+
   return (
-    <div>DocTitleOne</div>
+    <div>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </div>
   )
 }
 
