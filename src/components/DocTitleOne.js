4332f6ce31e4d299c749bac3a586e01c9a9d1a1d
ZikaZaki <17160061@su.edu.ye>
2023-05-31 00:46:20 +0300

    update DocTitleOne with useDocumentTitle custom-hook

--- src/components/DocTitleOne.js
+++ src/components/DocTitleOne.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react"
+import useDocumentTitle from "../hooks/useDocumentTitle"
 
 function DocTitleOne() {
   const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    document.title = `Count ${count}`
-  }, [count])
+  useDocumentTitle(count)
 
   return (
     <div>
