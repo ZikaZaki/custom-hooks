e284e5399e3c3b76dca5eac149c888e8f995d7c5
ZikaZaki <17160061@su.edu.ye>
2023-06-01 00:22:44 +0300

    add CustomInput to ParentContainer

--- src/components/ParentContainer.js
+++ src/components/ParentContainer.js
@@ -1,11 +1,20 @@
 import React, { useState, useRef } from "react"
+import CustomInput from "./CustomInput"
 
 function ParentContainer() {
   const [value, setValue] = useState("red")
   const inputRef = useRef()
 
   return (
-    <div>ParentContainer</div>
+    <>
+      <CustomInput
+        ref={inputRef}
+        value={value}
+        onChange={e => setValue(e.target.value)} 
+      />
+      <br />
+      <button onClick={() => inputRef.current.focus()}>Focus</button>
+    </>
   )
 }
 
