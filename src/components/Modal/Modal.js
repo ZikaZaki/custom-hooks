8ad031add4e2ac5984f989a280381059148931b8
ZikaZaki <17160061@su.edu.ye>
2023-06-01 00:08:16 +0300

    add summary comment

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,15 @@
 import React, { forwardRef, useImperativeHandle, useState } from "react"
-
+/**
+----------------------------------------------------------------------------------------------------------------
+* useImperativeHandle is a React hook that allows you to expose certain functions to parent components.
+*  It accepts two arguments: a reference to the component, and a function that returns an object containing 
+*  the functions that you want to expose. In this case, we want to expose the openModal function, so we
+*  pass a reference to the function to the second argument.
+*  The first argument to useImperativeHandle is a reference to the component. In this case, we want to
+*  pass a reference to the Modal component.
+*  The second argument is a function that returns an object containing the functions that we want to expose.
+----------------------------------------------------------------------------------------------------------------
+ */
 function Modal(props, ref) {
   const [modalState, setModalState] = useState(false);
 
