254d1e9641e4c6f0b4074bda6abb44cc12c4863e
ZikaZaki <17160061@su.edu.ye>
2023-06-01 01:40:25 +0300

    update ConfirmationModal component

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,8 +1,22 @@
 import React from "react"
 
-function ConfirmationModal() {
+function ConfirmationModal({ isOpen, onClose }, ref) {
+  if(!isOpen) return null
+
   return (
-    <div>ConfirmationModal</div>
+    <div className="modal" ref={ref}>
+      <button className="close-btn" onClick={onClose}>
+        &times;
+      </button>
+      <div className="modal-header">
+        <h1>Title</h1>
+      </div>
+      <div className="modal-body">Do you confirm?</div>
+      <div className="modal-footer">
+        <button className="confirm-btn" onClick={onClose}>Yes</button>
+        <button className="deny-btn" onClick={onClose}>No</button>
+      </div>
+    </div>
   )
 }
 
