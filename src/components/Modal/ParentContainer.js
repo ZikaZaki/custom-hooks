8e260dc4e78d124aa6e6752d69268dc31f08623b
ZikaZaki <17160061@su.edu.ye>
2023-05-31 23:45:24 +0300

    add useRef hook

--- src/components/Modal/ParentContainer.js
+++ src/components/Modal/ParentContainer.js
@@ -1,8 +1,18 @@
-import React from "react"
+import React, { useRef } from "react"
 
 function ParentContainer() {
+  const modalRef = useRef()
+
+  const handleOpenModal = () => {
+    modalRef.current.openModal()
+  }
+
+  console.log("Parent Container Rendered")
+
   return (
-    <div>ParentContainer</div>
+    <div>
+      <p>Parent Component</p>
+    </div>
   )
 }
 
