dce34121e120f528eb285a8614613941e885c3d5
ZikaZaki <17160061@su.edu.ye>
2023-06-02 23:49:38 +0300

    update PostsList with useReducer hook

--- src/components/Post/PostsList.js
+++ src/components/Post/PostsList.js
@@ -1,6 +1,33 @@
-import React from "react"
+import React, { useState, useReducer } from "react"
+
+const initialSatate = {
+    posts: [],
+    loading: true,
+    error: ""
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "FETCH_POSTS_SUCCESS":
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false,
+        error: ""
+      }
+    case "FETCH_POSTS_FAILURE":
+      return {
+        ...state,
+        posts: [],
+        loading: false,
+        error: action.payload
+      }
+    default: return state
+  }
+}
 
 function PostsList() {
+  
   return (
     <div>PostsList</div>
   )
