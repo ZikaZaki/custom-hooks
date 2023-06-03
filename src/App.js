37b2e34c171ec1a0eb6e6c4773dbd1501835e8ff
ZikaZaki <17160061@su.edu.ye>
2023-06-04 02:44:37 +0300

    add Person to App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ import DocTitleTwo from "./components/DocTitleTwo";
 import ParentContainer from "./components/ParentContainer";
 import PostsList from "./components/Post/PostsList";
 import UserForm from "./components/UserForm";
-
+import React, { useState } from "react";
+import { Person, usePersonProps } from "./components/Person";
 function App() {
+  const [count, setCount] = useState(0);
+  const inc = () => setCount(count + 1);
+  const dec = () => setCount(count - 1);
+
+  const personProps = usePersonProps();
+
   return (
     <div className="App">
       {/* <DocTitleOne />
@@ -18,7 +25,14 @@ function App() {
       {/* <UserForm /> */}
       {/* <ParentContainer /> */}
       {/* <ParentContainer /> */}
-      <PostsList />
+      {/* <PostsList /> */}
+      <div>Count: {count}</div>
+      <div>
+        <button onClick={inc}>+</button>
+        <button onCanPlay={dec}>-</button>
+      </div>
+      
+      <Person {...personProps} />
     </div>
   );
 }
