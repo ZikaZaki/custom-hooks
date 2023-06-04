3ecd7ec44e18a56fc96bc4fcbd7bef111fbc21d2
ZikaZaki <17160061@su.edu.ye>
2023-06-04 23:52:41 +0300

    add useDebugValue & useEffect hooks

---
+++ src/hooks/useLogger.js
@@ -0,0 +1,10 @@
+import { useDebugValue, useEffect } from "react"
+
+function useLogger(value) {
+  useDebugValue(value)
+  useEffect(() => {
+    console.log(value)
+  }, [value])
+}
+
+export default useLogger
