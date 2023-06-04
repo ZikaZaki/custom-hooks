556ce8e0c9080cafdf313a0925b6a0407e5b4c0a
ZikaZaki <17160061@su.edu.ye>
2023-06-04 23:43:35 +0300

    update Person component

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-export type PersonProps = {
-    firstName: string, setFirstName: (value: string) => void,
-    lastName: string, setLastName: (value: string) => void,
-}
+// export type PersonProps = {
+//     firstName: string, setFirstName: (value: string) => void,
+//     lastName: string, setLastName: (value: string) => void,
+// }
 
-export const Person = ({ firstName, setFirstName, lastName, setLastName }: PersonProps) => {
+export const Person = ({ firstName, setFirstName, lastName, setLastName }) => {
     return (
         <>
             <label htmlFor="firstName">First Name</label>
