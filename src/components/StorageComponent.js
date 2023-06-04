33ef89d419f1aee8eb4f8af6ec865a502bba2382
ZikaZaki <17160061@su.edu.ye>
2023-06-05 02:46:23 +0300

    update StorageComponent

--- src/components/StorageComponent.js
+++ src/components/StorageComponent.js
@@ -1,9 +1,17 @@
 import React from "react"
+import { useSessionStorage, useLocalStorage } from "../hooks/useStorage"
 
-function StorageComponent() {
+export default function StorageComponent() {
+  const [name, setName, removeName] = useSessionStorage("name", "ZikaZaki")
+  const [age, setAge, removeAge] = useLocalStorage("age", "24")
+
   return (
-    <div>StorageComponent</div>
+    <div>
+        <div>name: {name} | age: {age}</div>
+        <button onClick={() => setName("Zack")}>Set Name</button>
+        <button onClick={() => setAge("28")}>Set Age</button>
+        <button onClick={removeName}>Remove Name</button>
+        <button onClick={removeAge}>Remove Age</button>
+    </div>
   )
 }
-
-export default StorageComponent
