603853a324252a22c830d9b8a0282e3cbc689b2e
ZikaZaki <17160061@su.edu.ye>
2023-06-05 00:00:20 +0300

    update summary comment

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react"
 import useLogger from "../hooks/useLogger"
 /**
- * useDebugValue Hook is 
+ * useDebugValue Hook is used to display a label for custom hooks in React DevTools.
+ * USE CASES:
+ * - Displaying a label for a custom hook
+ * - Displaying a label for a custom hook with a custom value
+ * - Displaying a label for a custom hook with a custom value and a custom formatter
+ * 
+ * @returns {JSX.Element}
  */
 function Test() {
   const [name, setName] = useState("")
