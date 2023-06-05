a8a481372e8af993c051aeb27e19dd30f6824f60
ZikaZaki <17160061@su.edu.ye>
2023-06-06 02:18:27 +0300

    add summary comment

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,9 @@
 import { useState, useEffect, useCallback, useDebugValue } from "react"
-
+/**
+ * useDebugValue is useful for debugging purposes. It displays a label for custom hooks in React DevTools.
+ * NOTE: useDebugValue is a no-op when not in development mode.
+ * NOTE: useDebugValue is only useful and visible when used as a label inside a custom hook.
+ */
 export function useLocalStorage(key, initialValue) {
     const [value, setValue] = useState(() => {
         const jsonValue = localStorage.getItem(key)
