29b04242f31c2144bb00fa7e1527ce8fc812e830
ZikaZaki <17160061@su.edu.ye>
2023-06-06 02:43:34 +0300

    comment out unnecessary comopnents

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       {/* <PostsList /> */}
       {/* <Test /> */}
       {/* <StorageComponent /> */}
-      <LocalStorageComp />
+      {/* <LocalStorageComp /> */}
     </div>
   );
 }
