af37ee8f36d6dda3801e78ea2b3372e4df408573
ZikaZaki <17160061@su.edu.ye>
2023-06-05 03:06:19 +0300

    update name initialValue with a function

--- src/components/StorageComponent.js
+++ src/components/StorageComponent.js
@@ -1,15 +1,15 @@
 import { useSessionStorage, useLocalStorage } from "../hooks/useStorage"
 
 export default function StorageComponent() {
-  const [name, setName, removeName] = useSessionStorage("name", "ZikaZaki")
+  const [name, setName, removeName] = useSessionStorage("name", function() {return "Zack Ali"})
   const [age, setAge, removeAge] = useLocalStorage("age", "24")
 
   return (
     <div>
         <div>name: {name} - age: {age}</div>
         <button onClick={() => setName("Zack")}>Set Name</button>
-        <button onClick={() => setAge("28")}>Set Age</button>
         <button onClick={removeName}>Remove Name</button>
+        <button onClick={() => setAge("28")}>Set Age</button>
         <button onClick={removeAge}>Remove Age</button>
     </div>
   )
