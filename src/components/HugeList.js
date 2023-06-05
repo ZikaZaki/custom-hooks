dd7632c2b9d6a7136e3c96460325408caf450ba5
ZikaZaki <17160061@su.edu.ye>
2023-06-06 02:42:44 +0300

    add input & list states

--- src/components/HugeList.js
+++ src/components/HugeList.js
@@ -1,8 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+
+const LIST_LENGTH = 20000
 
 function HugeList() {
+  const [input, setInput] = useState("")
+  const [list, setList] = useState([])
+  
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+    const newList = []
+    for (let i = 0; i < LIST_LENGTH; i++) {
+        newList.push(e.target.value)
+    }
+    setList(newList)
+  }
+
+
   return (
-    <div>HugeList</div>
+    <div>
+        <input type="text" value={input} onChange={handleInputChange} />
+        {list && list.map((item, index) => {
+            return <div key={index}>{item}</div>
+        })}
+    </div>
   )
 }
 
