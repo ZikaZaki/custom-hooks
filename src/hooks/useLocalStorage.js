cfd9110e2a0bcd00341a83bc6f1be1af73d35b12
ZikaZaki <17160061@su.edu.ye>
2023-06-06 02:30:59 +0300

    add getValueSlowly function

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,18 @@
 import { useState, useEffect, useCallback, useDebugValue } from "react"
 /**
+ * --------------------------------------------------------------------------------------------------------
  * useDebugValue is useful for debugging purposes. It displays a label for custom hooks in React DevTools.
  * NOTE: useDebugValue is a no-op when not in development mode.
  * NOTE: useDebugValue is only useful and visible when used as a label inside a custom hook.
+ * --------------------------------------------------------------------------------------------------------
+ * CAREFUL WHEN IN PRODUCTION:
+ * if you use useDebugValue in a production build, make sure to add a second argument to it which is a function,
+ * so that the function is only called in development mode.
+ * EXAMPLE:------------------------------------------------------------------
+ * useDebugValue("hi ZikaZaki", () => process.env.NODE_ENV === "development")
+ * useDebugValue({ key, value }, () => process.env.NODE_ENV === "development")
+ * useDebugValue(value, (v) => getValueSlowly(v))
+ * --------------------------------------------------------------------------------------------------------
  */
 export function useLocalStorage(key, initialValue) {
     const [value, setValue] = useState(() => {
@@ -17,7 +27,9 @@ export function useLocalStorage(key, initialValue) {
     })
 
     // Visible only when used as a label inside custom-hooks.
-    useDebugValue([key, value])
+    useDebugValue("hi ZikaZaki")
+    useDebugValue({ key, value })
+    useDebugValue(value, v => getValueSlowly(v))
 
     useEffect(() => {
 
@@ -30,4 +42,9 @@ export function useLocalStorage(key, initialValue) {
     }, [])
 
     return [value, setValue, remove]
+}
+
+function getValueSlowly(value) {
+    for (let i = 0; i < 1000000000; i++) { }
+    return value
 }
