8723530c0847f26359a50a013b4b3f7d6a6ff762
ZikaZaki <17160061@su.edu.ye>
2023-06-07 01:07:21 +0300

    add useMemo hook

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,16 @@
 import { useMemo } from "react"
 
-function List() {
-  return (
-    <div>List</div>
-  )
+function List({ input }) {
+  const LIST_LENGTH = 20000
+  const list = useMemo(() => {
+    const l = []
+    for (let i = 0; i < LIST_LENGTH; i++) {
+      l.push(<div key={i}>{input}</div>)
+    }
+    return l
+  }, [input])
+
+  return list
 }
 
 export default List
