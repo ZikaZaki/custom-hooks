5467faa7c1df760028da3dd2469aaa043e93de3e
ZikaZaki <17160061@su.edu.ye>
2023-06-06 03:01:21 +0300

    add useTransition hook

--- src/components/HugeList.js
+++ src/components/HugeList.js
@@ -28,25 +28,30 @@ import React, { useState, useTransition } from "react"
 const LIST_LENGTH = 20000
 
 function HugeList() {
+  const [isPending, startTransition] = useTransition()
   const [input, setInput] = useState("")
   const [list, setList] = useState([])
   
   const handleInputChange = (e) => {
     setInput(e.target.value)
-    const newList = []
-    for (let i = 0; i < LIST_LENGTH; i++) {
-        newList.push(e.target.value)
-    }
-    setList(newList)
+
+    /** Everything in the startTransition method is a low-priority */
+    startTransition(() => {
+        const newList = []
+        for (let i = 0; i < LIST_LENGTH; i++) {
+            newList.push(e.target.value)
+        }
+        setList(newList)
+    })
   }
 
 
   return (
     <div>
         <input type="text" value={input} onChange={handleInputChange} />
-        {list && list.map((item, index) => {
+        {isPending ? list && list.map((item, index) => {
             return <div key={index}>{item}</div>
-        })}
+        }) : null}
     </div>
   )
 }
