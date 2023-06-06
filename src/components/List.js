35cb761247f9cbcab410151ba228992d38610e02
ZikaZaki <17160061@su.edu.ye>
2023-06-07 02:08:45 +0300

    add summary comment

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,11 @@
 import { useMemo, useDeferredValue, useEffect } from "react"
+/**
+ * useDeferredValue is a new React hook that allows you to defer the update of a value until the next render.
+ * It accepts two arguments: the value to defer and an optional configuration object.
+ * The configuration object has a timeout property that allows you to specify the maximum amount of time
+ * React should wait before updating the deferred value.
+ * USE CASES:
+ */
 
 function List({ input }) {
   const LIST_LENGTH = 20000
