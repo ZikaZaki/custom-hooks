8f26059a4d99717e39c63e6391caa3001bd8846e
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-06-25 23:14:02 +0300

    Update useCounter.js

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,18 +1,18 @@
-import { useState }from "react"
+import { useState }from "react";
 
 function useCounter(value, initialCount = 0) {
-  const [count, setCount] = useState(initialCount)
+  const [count, setCount] = useState(initialCount);
   const increment = () => {
-    setCount(prevCount => prevCount + value)
-  }
+    setCount(prevCount => prevCount + value);
+  };
   const decrement = () => {
-    setCount(prevCount => prevCount - value)
-  }
+    setCount(prevCount => prevCount - value);
+  };
   const reset = () => {
-    setCount(initialCount)
-  }
+    setCount(initialCount);
+  };
 
-  return [count, increment, decrement, reset]
+  return [count, increment, decrement, reset];
 }
 
-export default useCounter
+export default useCounter;
